055e13a5b527c1529b9cf3dfe0190f74e4395778
Update56 <heupdate56@gmail.com>
2022-12-06 16:06:15 +0700

    Revert "Removed the unnecessary line"
    This reverts commit 736562f77ddc1e155d0469492e4a7b81826252b4.

--- learning/Game1.cs
+++ learning/Game1.cs
@@ -81,6 +81,7 @@ namespace learning
                 
                     if (keyboardState.IsKeyDown(Keys.Left))
                     {
+                        snake[0] = new Vector2 { X = snake[0].X - speed, Y = snake[0].Y};
                         pos.X -= speed;
                         rndcolor = GetRandomColor();
                     }
